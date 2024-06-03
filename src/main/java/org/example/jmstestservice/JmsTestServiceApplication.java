61ab5cac3ef137c3851bf5b8f428cdded72fe929
Maksim Ushakov <dmeaaxd@gmail.com>
2024-06-03 18:33:50 +0300

    Initial commit

---
+++ src/main/java/org/example/jmstestservice/JmsTestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.jmstestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsTestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmsTestServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jmstestservice/JmsTestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jmstestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsTestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
